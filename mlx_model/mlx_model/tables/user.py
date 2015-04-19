f012d43781fd8cb4a7dbe125cf3cdd3bbc47bc03
Guido Accardo <gaccardo@gmail.com>
2015-04-19 20:35:09 -0300

    email fields

--- mlx_model/mlx_model/tables/user.py
+++ mlx_model/mlx_model/tables/user.py
@@ -11,6 +11,13 @@ class User(MLXBase):
     firstname = Column(String(50), nullable=False)
     lastname = Column(String(50), nullable=False)
     picture = Column(LargeBinary, nullable=True)
+    email = Column(String(150), nullable=False)
     password = Column(st.password.PasswordType(
         schemes=['md5_crypt'],
     ))
+
+    def generate_token(self):
+        return '4848484848484'
+
+    def verify_credentials(self, email, password):
+        return True
