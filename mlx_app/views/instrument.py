b66b2260ac3bb5b4cc4f0d22ffad50d33b380b08
Guido Accardo <gaccardo@gmail.com>
2015-04-19 19:58:22 -0300

    get a single instrument

--- mlx_app/views/instrument.py
+++ mlx_app/views/instrument.py
@@ -2,10 +2,9 @@ from mlx_app import app
 from mlx_model.mlx_model.tables import instrument
 from mlx_model.mlx_model import session
 from settings import settings
-from flask import jsonify
+from flask import jsonify, Response
 
-
-@app.route('/mlx/api/%s/instrument' % settings.VERSION)
+@app.route('%s/instrument' % settings.BASE_URL)
 def get_instruments():
     cs = session.CreateSession()
     se = cs.get_session()
@@ -19,4 +18,22 @@ def get_instruments():
     for inst in search:
         instruments['instruments'].append(inst.as_dict())
 
+    se.close()
     return jsonify(instruments)
+
+
+@app.route('%s/instrument/<int:id>' % settings.BASE_URL)
+def get_instrument(id):
+    cs = session.CreateSession()
+    se = cs.get_session()
+
+    search = se.query(
+        instrument.Instrument
+    ).filter(
+        instrument.Instrument.id == id
+    ).first()
+
+    se.close()
+    if search is None:
+        return Response('Not Found', 404)
+    return jsonify(search.as_dict())
