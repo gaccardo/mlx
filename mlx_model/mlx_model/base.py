0e1ff18a5b9343ea93daf601aa17f9d056d54eb9
Guido Accardo <gaccardo@gmail.com>
2015-04-20 09:35:56 -0300

    a few new properties for base

--- mlx_model/mlx_model/base.py
+++ mlx_model/mlx_model/base.py
@@ -6,6 +6,21 @@ declarative_base = lambda cls: real_declarative_base(cls=cls)
 
 @declarative_base
 class MLXBase(object):
+    __hide_columns__ = []
+
+    @property
+    def columns(self):
+        """Return a list with the names of the entity columns."""
+        return [c.name for c in self.__table__.columns]
+
+    @property
+    def columnitems(self):
+        """Return dict representation of entity columns."""
+        res = {}
+        for column in self.columns:
+            if column not in self.__hide_columns__:
+                res[column] = getattr(self, column)
+        return res
 
     def __repr__(self):
         """String representaion of the class."""
